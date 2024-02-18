d4f7856048e5d273185b2c3100006796b42998e0
Jacob Krch <jekrch@gmail.com>
2024-02-18 15:31:56 -0600

    update eurovision ranker screenshots

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Helvetica', 'Arial', 'sans-serif'],
+        //sans: ['Helvetica', 'Arial', 'sans-serif'],
       },
     },
   },
