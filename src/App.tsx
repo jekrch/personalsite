eca3597a77b89c46b22e86a0b2db5923655b04db
jekrch <jekrch@gmail.com>
2024-10-30 17:08:17 -0500

    dev port and update render

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import LogicLectures from "./components/LogicLectures"
 import ChordBuildr from "./components/ChordBuildr"
 import Music from "./components/Music"
@@ -9,21 +9,28 @@ import SocialIconBar from "./components/SocialIconBar"
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import "./App.css"
 
-ReactDOM.render(
-    <div className="pb-[2em] min-h-screen h-full relative">
-        <AppNavbar />
-        <div className="app content-max-width">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/logiclectures" element={<LogicLectures />} />
-                    <Route path="/chordbuildr" element={<ChordBuildr />} />
-                    <Route path="/eurovision-ranker" element={<EurovisionRanker />} />
-                    <Route path="/music" element={<Music />} />
-                </Routes>
-            </Router>
-        </div>
-        <SocialIconBar/>
-    </div>,
-    document.getElementById('root')
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Failed to find the root element');
+}
+
+const root = createRoot(rootElement);
+
+root.render(
+  <div className="pb-[2em] min-h-screen h-full relative">
+    <AppNavbar />
+    <div className="app content-max-width">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/logiclectures" element={<LogicLectures />} />
+          <Route path="/chordbuildr" element={<ChordBuildr />} />
+          <Route path="/eurovision-ranker" element={<EurovisionRanker />} />
+          <Route path="/music" element={<Music />} />
+        </Routes>
+      </Router>
+    </div>
+    <SocialIconBar/>
+  </div>
 );
